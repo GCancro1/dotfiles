b5f6739e2a83b2741b6e9e20f4abf5ad8cdb19a7
GCancro1 <george.cancro@yahoo.com>
2026-08-10 19:07:56 -0400

    may want to revert this

---
+++ .config/hypr/monitors.lua
@@ -0,0 +1,15 @@
+hl.monitor({
+  output = "DP-2",
+  mode = "3840x2160@70",
+  position = "0x0",
+  scale = 1.5,
+  bitdepth = 10,
+})
+
+hl.monitor({
+  output = "DP-1",
+  mode = "2560x1440@165.08",
+  position = "2560x0",
+  scale = 1,
+  bitdepth = 10,
+})
